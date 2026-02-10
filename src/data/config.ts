d6c4ecd611b6c47712526f3a210c68c99bfbae72
AMendoza <allanmaleman@gmail.com>
2026-02-10 11:12:03 +0800

    Feat: Add Footer and Header to the project

---
+++ src/data/config.ts
@@ -0,0 +1,55 @@
+// Global variables
+export const siteLang = "en-US";
+export const siteCurrency = "USD";
+export const siteDomain = "thinkglobalconsultants.com";
+export const siteName = "Think Global Consultants";
+export const siteSlogan = "Supporting those most in need";
+export const themeColor = "#157f71";
+export const firstTitle = "Think";
+export const secondTitle = "Global Consultants";
+
+export const phone = {
+	href: "tel:+123456789",
+	label: "(123) 456-789",
+};
+
+// Updated email address
+export const email = {
+	href: "mailto:example@gmail.com",
+	label: "example@gmail.com",
+};
+
+
+// Updated address information
+export const address = {
+	street: "1234 Space Street",
+	city: "Galaxy City",
+	zip: "12345",
+	state: "Milky Way",
+	country: "Space",
+};
+
+
+// Updated social media links
+export const socialMedia = {
+	facebook: {
+		url: "https://facebook.com",
+		label: "Facebook",
+		icon: "facebook",
+	},
+	twitter: {
+		url: "https://twitter.com",
+		label: "Twitter",
+		icon: "twitter",
+	},
+	instagram: {
+		url: "https://instagram.com",
+		label: "Instagram",
+		icon: "instagram",
+	},
+	youtube: {
+		url: "https://youtube.com",
+		label: "YouTube",
+		icon: "youtube",
+	},
+};
---
+++ src/data/menus.js
@@ -0,0 +1,65 @@
+export const menuMain = [
+    {
+        name: "Home",
+        url: "/",
+        highlighted: false,
+    },
+    {
+        name: "About Us",
+        url: "/about-us",
+        highlighted: false,
+    },
+    {
+        name: "Host Info",
+        url: "/hostInfo",
+        highlighted: false,
+    },
+    {
+        name: "Stories",
+        url: "/stories",
+        highlighted: false,
+    },
+    {
+        name: "Contact",
+        url: "/contact",
+        highlighted: false,
+    },
+    {
+        name: "Ways to Support",
+        url: "/support",
+        highlighted: false,
+    },
+    {
+        name: "Donate",
+        url: "https://www.givesendgo.com/missionarytechsupport",
+        highlighted: true,
+    },
+];
+
+export const footerPrimary = [
+    {
+        title: "Navigatie",
+        items: [
+            { name: "home", url: "/" },
+        ],
+    },
+];
+
+export const footerSecondary = [
+    {
+        title: "support",
+        items: [
+            { name: "pricing", url: "/pricing" },
+        ],
+    },
+];
+
+export const footerTertiary = [
+    {
+        title: "Links",
+        items: [
+            { name: "privacybeleid", url: '/privacy-policy' },
+            { name: "api status", url: "/api-status" },
+        ],
+    },
+];
